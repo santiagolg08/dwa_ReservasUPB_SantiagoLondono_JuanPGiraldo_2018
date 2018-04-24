bdcb1d4e9b6d6da774c088ceec67aa074c54e87b
DESKTOP-LO788SI\Juan Pablo <jupa.1996@hotmail.com>
2018-04-24 11:35:29 -0500

    Creacion de componente inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthComponent } from './auth/auth.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { AuthComponent } from './auth/auth.component';
     AppComponent,
     HeaderComponent,
     AuthComponent,
+    InicioComponent,
   ],
   imports: [
     BrowserModule
