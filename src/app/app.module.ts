9201c7e15a39b0b4844d193eca91e42c91c8fa13
Desarrollo1\Desarrollador1 <slondono@gisolutions.com.co>
2018-04-28 15:33:45 -0500

    Guardar registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
+import {environment} from '../environments/environment';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthComponent } from './auth/auth.component';
 
+import { AngularFireModule } from 'angularfire2';
+// for AngularFireDatabase
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
+// for AngularFireAuth
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import {AuthService} from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +26,19 @@ import { AuthComponent } from './auth/auth.component';
     AuthComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot(
+      [
+        { path: "", component: AuthComponent}
+      ]
+    )
+    
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
