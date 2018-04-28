48096f923ef31faf662dd1af2866aabc6b4b7901
DESKTOP-LO788SI\Juan Pablo <jupa.1996@hotmail.com>
2018-04-28 12:46:45 -0500

    Mapa con ubicación de UPB

--- src/app/escenarios/escenarios.component.ts
+++ src/app/escenarios/escenarios.component.ts
@@ -10,6 +10,7 @@ export class EscenariosComponent implements OnInit {
   title: string = 'My first AGM project';
   lat: number = 6.242052007104904;
   lng: number = -75.589807519906;
+  zoom:number = 17;
 
   constructor() { }
 
