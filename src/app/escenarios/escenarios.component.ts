a980a364a7c7b238becbb27399c02579b8a6a3f9
DESKTOP-LO788SI\Juan Pablo <jupa.1996@hotmail.com>
2018-04-30 13:04:08 -0500

    Escenarios con mapa Ok y select OK (Falta imagenes de escenarios)

--- src/app/escenarios/escenarios.component.ts
+++ src/app/escenarios/escenarios.component.ts
@@ -12,33 +12,27 @@ export class EscenariosComponent implements OnInit {
 	zoom:number = 17;
 	
 	escenario_index:number = 0;
-	// select_escenarios:any;
-  
-  // lat_marker: number = 6.242147994160795;
-  // lng_marker: number = -75.59137929438873;
-
-
 
   markers: marker[] = [
-	  {	nombre_escenario:"cancha_fundadores",
+	  {	nombre_escenario:"Cancha Fundadores",
 		  lat: 6.242182866847922,
 			lng: -75.59138774871826,
 			src_img : "../../assets/img/sprites/sprite_1/medal.png",
 		},
 	  {
-			nombre_escenario:"cancha_bloque19",
+			nombre_escenario:"Cancha Bloque19",
 			lat: 6.241788699429994,
 			lng: -75.58904748395435,
 			src_img : "../../assets/img/sprites/sprite_1/menu.png",
 	  },
 	  {
-			nombre_escenario:"polideportivo",
+			nombre_escenario:"Polideportivo",
 			lat: 6.241761590222094,
 			lng: -75.59048652648926,
 			src_img : "../../assets/img/sprites/sprite_1/profile.png",
 		 },
 		 {
-			nombre_escenario:"cancha_tenis",
+			nombre_escenario:"Cancha de Tenis",
 			lat: 6.241740705871343,
 			lng: -75.5888496710395,
 			src_img : "../../assets/img/sprites/sprite_1/favicon.png",
@@ -52,16 +46,6 @@ export class EscenariosComponent implements OnInit {
   ngOnInit() {
 		
   }
-	
-	actualizarEscenario(){
-		var esc = document.getElementById("escenario_seleccionado");
-		var esccc = esc.nodeValue;
-		console.log(esccc);
-	}
-
-  cambiarEscenario(){
-    console.log("cambiando marker");
-	}
 	
 	onChange(escenario_index) {
     this.escenario_index = escenario_index;
