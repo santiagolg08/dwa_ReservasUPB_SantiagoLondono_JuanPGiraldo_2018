c9ece6371b5045408e930a478be6faec5d690758
Desarrollo1\Desarrollador1 <slondono@gisolutions.com.co>
2018-05-27 12:18:14 -0500

    Arreglo merge

--- src/app/services/escenarios.service.ts
+++ src/app/services/escenarios.service.ts
@@ -7,23 +7,65 @@ import { query } from '@angular/animations';
 @Injectable()
 export class EscenariosService {
 
-  escenariosList : AngularFireList<Escenarios> = null;
-  lstEscenarios : Escenarios[] = [];
+  escenariosList: AngularFireList<Escenarios> = null;
+  lstEscenarios: Escenarios[] = [];
   listEscenarios: any;
+  public lista_src_escenarios = [
+    [
+      "../../assets/img/fotos_escenarios/escenario_fundadores_1.JPG",
+      "../../assets/img/fotos_escenarios/escenario_fundadores_2.JPG",
+      "../../assets/img/fotos_escenarios/escenario_fundadores_3.JPG",
+      "../../assets/img/fotos_escenarios/escenario_fundadores_4.JPG",
+    ],
+    [
+      "../../assets/img/fotos_escenarios/escenario_cancha_bloque19_1.JPG",
+      "../../assets/img/fotos_escenarios/escenario_cancha_bloque19_2.JPG",
+      "../../assets/img/fotos_escenarios/escenario_cancha_bloque19_3.JPG",
+      "../../assets/img/fotos_escenarios/escenario_cancha_bloque19_4.JPG",
+    ],
+    [
+      "../../assets/img/fotos_escenarios/escenario_polideportivo_1.JPG",
+      "../../assets/img/fotos_escenarios/escenario_polideportivo_2.JPG",
+      "../../assets/img/fotos_escenarios/escenario_polideportivo_3.JPG",
+      "../../assets/img/fotos_escenarios/escenario_polideportivo_4.JPG",
+    ],
+    [
+      "../../assets/img/fotos_escenarios/escenario_tenis_1.JPG",
+      "../../assets/img/fotos_escenarios/escenario_tenis_2.JPG",
+      "../../assets/img/fotos_escenarios/escenario_tenis_3.JPG",
+      "../../assets/img/fotos_escenarios/escenario_tenis_4.JPG",
+    ],
+    [
+      "../../assets/img/fotos_escenarios/escenario_placas_1.JPG",
+      "../../assets/img/fotos_escenarios/escenario_placas_2.JPG",
+      "../../assets/img/fotos_escenarios/escenario_placas_3.JPG",
+      "../../assets/img/fotos_escenarios/escenario_placas_4.JPG",
 
-  constructor(private firebase: AngularFireDatabase) { 
+    ],
+  ]
+
+  constructor(private firebase: AngularFireDatabase) {
     this.escenariosList = this.firebase.list('Escenarios');
   }
 
-   get listaEscenarios(): Escenarios[]{
-     return this.lstEscenarios;
-   }
+  get listaEscenarios(): Escenarios[] {
+    return this.lstEscenarios;
+  }
 
-   getListaEscenarios() {
+  getListaEscenarios() {
     return this.escenariosList;
-   }
-   
-   obtenerEscenarios(){
+  }
+
+  obtenerEscenarios() {
     return this.listEscenarios = this.firebase.list('Escenarios').valueChanges();
-   }
+  }
+
+  prueba(nombre_prenda: string) {
+    return nombre_prenda;
+  }
+
+  getEscenarios(index) {
+    return this.lista_src_escenarios[index];
+  }
+
 }
