0cb06e74ba8ea90372b2bc5721ed99ed7f137ba8
DESKTOP-LO788SI\Juan Pablo <jupa.1996@hotmail.com>
2018-05-27 13:51:07 -0500

    Lista de imagenes OK para cada escenario

--- src/app/services/escenarios.service.ts
+++ src/app/services/escenarios.service.ts
@@ -13,33 +13,33 @@ export class EscenariosService {
   public lista_src_escenarios = [
     [
       "../../assets/img/fotos_escenarios/escenario_fundadores_1.JPG",
-      "../../assets/img/fotos_escenarios/escenario_fundadores_2.JPG",
       "../../assets/img/fotos_escenarios/escenario_fundadores_3.JPG",
-      "../../assets/img/fotos_escenarios/escenario_fundadores_4.JPG",
+      "../../assets/img/fotos_escenarios/escenario_fundadores_5.JPG",
+      "../../assets/img/fotos_escenarios/escenario_fundadores_7.JPG",
     ],
     [
-      "../../assets/img/fotos_escenarios/escenario_cancha_bloque19_1.JPG",
-      "../../assets/img/fotos_escenarios/escenario_cancha_bloque19_2.JPG",
       "../../assets/img/fotos_escenarios/escenario_cancha_bloque19_3.JPG",
+      "../../assets/img/fotos_escenarios/escenario_cancha_bloque19_2.JPG",
       "../../assets/img/fotos_escenarios/escenario_cancha_bloque19_4.JPG",
+      "../../assets/img/fotos_escenarios/escenario_cancha_bloque19_1.JPG",
     ],
     [
-      "../../assets/img/fotos_escenarios/escenario_polideportivo_1.JPG",
       "../../assets/img/fotos_escenarios/escenario_polideportivo_2.JPG",
-      "../../assets/img/fotos_escenarios/escenario_polideportivo_3.JPG",
       "../../assets/img/fotos_escenarios/escenario_polideportivo_4.JPG",
+      "../../assets/img/fotos_escenarios/escenario_polideportivo_7.jpg",
+      "../../assets/img/fotos_escenarios/escenario_polideportivo_6.JPG",
     ],
     [
       "../../assets/img/fotos_escenarios/escenario_tenis_1.JPG",
-      "../../assets/img/fotos_escenarios/escenario_tenis_2.JPG",
       "../../assets/img/fotos_escenarios/escenario_tenis_3.JPG",
+      "../../assets/img/fotos_escenarios/escenario_tenis_2.JPG",
       "../../assets/img/fotos_escenarios/escenario_tenis_4.JPG",
     ],
     [
-      "../../assets/img/fotos_escenarios/escenario_placas_1.JPG",
-      "../../assets/img/fotos_escenarios/escenario_placas_2.JPG",
-      "../../assets/img/fotos_escenarios/escenario_placas_3.JPG",
       "../../assets/img/fotos_escenarios/escenario_placas_4.JPG",
+      "../../assets/img/fotos_escenarios/escenario_placas_3.JPG",
+      "../../assets/img/fotos_escenarios/escenario_placas_5.JPG",
+      "../../assets/img/fotos_escenarios/escenario_placas_7.JPG",
 
     ],
   ]
